ca1f812f8954fc032fb7c7382b03a0e6730b384e
naeemnoman <naeemnoman0419@gmail.com>
2024-09-17 13:27:17 +0600

    my project

--- app/src/main/java/com/example/barca/MainActivity.kt
+++ app/src/main/java/com/example/barca/MainActivity.kt
@@ -8,40 +8,42 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.barca.Adapter.PlayerAdapter
+import com.example.barca.databinding.ActivityMainBinding
 import com.example.barca.databinding.BarcaitemBinding
+import com.example.barca.model.Activity_details
 import com.example.barca.model.Information
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: BarcaitemBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var PlayerAdapter: PlayerAdapter
     val information = ArrayList<Information>()
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = BarcaitemBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
-      setContentView(R.layout.activity_main)
 
 
         binding.BarcaDetails.layoutManager = LinearLayoutManager(this)
 
-        information.add(Information("Lionel Messi", "RWF", 1000.0, R.drawable.messi,"GOAT"))
-        information.add(Information("Luis Suarez", "CF", 1000.0, R.drawable.suarez,"Undarated"))
-        information.add(Information("Neymar Jr", "LWF", 1000.0, R.drawable.neymar,"Magic"))
-        information.add(Information("Andress Iniesta", "CMF", 1000.0, R.drawable.iniesta,"Assister"))
-        information.add(Information("Xavi", "CMF", 1000.0, R.drawable.xavi,"Technic"))
-        information.add(Information("Busquest", "DMF", 1000.0, R.drawable.busi,"passing"))
-        information.add(Information("Jordi Alba", "LB", 1000.0, R.drawable.alaba,"Talent"))
-        information.add(Information("Carlos Puqol", "CMF", 1000.0, R.drawable.puyol,"wall"))
-        information.add(Information("Gerrad Pique", "CMF", 1000.0, R.drawable.pique,"deffence"))
-        information.add(Information("Dani Alves", "CMF", 1000.0, R.drawable.dani,"Strenght"))
-        information.add(Information("Victor Valbes", "CMF", 1000.0, R.drawable.victor,"great"))
+        information.add(Information("Lionel Messi", "RWF", 1000.0, R.drawable.messi, "GOAT"))
+        information.add(Information("Luis Suarez", "CF", 1000.0, R.drawable.suarez, "Undarated"))
+        information.add(Information("Neymar Jr", "LWF", 1000.0, R.drawable.neymar, "Magic"))
+        information.add(Information("Andress Iniesta", "CMF", 1000.0, R.drawable.iniesta, "Assister"))
+        information.add(Information("Xavi", "CMF", 1000.0, R.drawable.xavi, "Technic"))
+        information.add(Information("Busquest", "DMF", 1000.0, R.drawable.busi, "passing"))
+        information.add(Information("Jordi Alba", "LB", 1000.0, R.drawable.alaba, "Talent"))
+        information.add(Information("Carlos Puqol", "CMF", 1000.0, R.drawable.puyol, "wall"))
+        information.add(Information("Gerrad Pique", "CMF", 1000.0, R.drawable.pique, "deffence"))
+        information.add(Information("Dani Alves", "CMF", 1000.0, R.drawable.dani, "Strenght"))
+        information.add(Information("Victor Valbes", "CMF", 1000.0, R.drawable.victor, "great"))
 
         PlayerAdapter = PlayerAdapter(information)
         binding.BarcaDetails.adapter = PlayerAdapter
 
-        PlayerAdapter.onClick = { information
-            val intent = Intent(this, PlayerDetails::class.java)
+        PlayerAdapter.onClick = {
+            information
+            val intent = Intent(this, Activity_details::class.java)
             intent.putExtra("player", information)
             intent.putExtra("postion", information)
             intent.putExtra("price", information)
@@ -51,10 +53,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.addBtn.setOnClickListener {
-            showP
 
 
-
-
+        }
     }
 }
